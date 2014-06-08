c309078940c1c3be967aeec74447bd46437e4d49
Peter MEUEL <peter@teriiehina.net>
2014-06-08 02:11:39 +0200

    Add STTwitter as a pod

---
+++ Pods/Pods-ParauParau-STTwitter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParauParau_STTwitter : NSObject
+@end
+@implementation PodsDummy_Pods_ParauParau_STTwitter
+@end
---
+++ Pods/Pods-ParauParau-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParauParau : NSObject
+@end
+@implementation PodsDummy_Pods_ParauParau
+@end
